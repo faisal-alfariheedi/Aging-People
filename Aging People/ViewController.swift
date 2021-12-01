b3dc2c6ce9de4b07c82c002ba6d4845be32e49e5
faisal alfariheedi <falfariheedi@gmail.com>
2021-12-01 13:25:02 +0300

    Initial Commit

---
+++ Aging People/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Aging People
+//
+//  Created by faisal on 26/04/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
