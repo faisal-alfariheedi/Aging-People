e1de9564785509844c67b3f7f0b4646e96923be9
faisal alfariheedi <falfariheedi@gmail.com>
2021-12-01 14:00:53 +0300

    fin

--- Aging People/ViewController.swift
+++ Aging People/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var table: UITableView!
+    var list=[["mark",50]]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        table.dataSource=self
+        table.delegate=self
+        list.removeAll()
+        for i in 0...15{
+            list.insert(["mark\(i)",Int.random(in: 1...100)], at: i)
+        }
+        table.reloadData()
         // Do any additional setup after loading the view.
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
 
 
 }
 
+extension ViewController: UITableViewDataSource,UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+                // set text label to the model that is corresponding to the row in array
+        cell.textLabel?.text = (list[indexPath.row][0] as! String)
+        cell.detailTextLabel?.text = "\(list[indexPath.row][1]) years old"
+        return cell
+    }
+    
+    
+    
+}
